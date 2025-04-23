b605f5d31ebe4fde6980ac1b9dd9a46ad34da506
tjdanqkr <tjdanqkr@gmail.com>
2025-04-23 18:02:57 +0900

    fix: enum lombok 적용

--- src/main/java/com/plus/service/user/domain/UserRole.java
+++ src/main/java/com/plus/service/user/domain/UserRole.java
@@ -1,16 +1,14 @@
 package com.plus.service.user.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
     USER("ROLE_USER"),
     ADMIN("ROLE_ADMIN");
 
     private final String value;
 
-    UserRole(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
 }
